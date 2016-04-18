cd8ab772c588b77259acf7da1701dcac1a29e9e7
SolarPolarMan <solarpolarman@gmail.com>
2016-04-18 15:36:01 +0100

    removed unused apikey code.

--- policeData.py
+++ policeData.py
@@ -3,19 +3,6 @@ from urllib.parse import quote
 from urllib import request
 import json
 
-if os.path.isfile("apikey.txt"):
-    pass
-else:
-    with open("apikey.txt") as f:
-        f.write(input("Please enter your google API Key. If you don't have one"\
-    " visit https://console.developers.google.com/apis/api/geocoding_backend/"))
-        f.close()
-
-with open("apikey.txt") as f:
-    key = f.read()
-    f.close()
-
-
 address = input("Address to search from: ")
 ur = quote("https://maps.googleapis.com/maps/api/geocode/json?address="+address, safe = "&/:?=")
 k = request.urlopen(ur).read().decode()
